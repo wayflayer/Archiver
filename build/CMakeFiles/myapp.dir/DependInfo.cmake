a9048f96e5df0b11c531cf1d063e94bcc2ca69b9
wayflayer <wayflayeeer@gmail.com>
2025-05-08 19:45:25 +0300

    убрал лишние файлы

---
+++ build/CMakeFiles/myapp.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wayflayer/developer/cpp/file_archiver/main.cpp" "CMakeFiles/myapp.dir/main.cpp.o" "gcc" "CMakeFiles/myapp.dir/main.cpp.o.d"
+  "/home/wayflayer/developer/cpp/file_archiver/src/Archiver.cpp" "CMakeFiles/myapp.dir/src/Archiver.cpp.o" "gcc" "CMakeFiles/myapp.dir/src/Archiver.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/myapp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myapp.dir/main.cpp.o"
+  "CMakeFiles/myapp.dir/main.cpp.o.d"
+  "CMakeFiles/myapp.dir/src/Archiver.cpp.o"
+  "CMakeFiles/myapp.dir/src/Archiver.cpp.o.d"
+  "myapp"
+  "myapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
